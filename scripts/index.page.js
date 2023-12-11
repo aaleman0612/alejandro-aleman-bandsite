81b5afba0e1524686b84628da34dcd4af7d964de
Alejandro Aleman <aaleman0612@gmail.com>
2023-12-10 20:13:31 -0500

    fixing my comments sectiion alignment

--- scripts/index.page.js
+++ scripts/index.page.js
@@ -1,61 +1,102 @@
-function createAndAppendElement(parent, elementInfo) {
-    let element = newElement(elementInfo);
-    parent.appendChild(element);
-    return element
+function createCommentElement(obj) {
+    const newTimestamp = document.createElement('p')
+    newTimestamp.classList.add('commnet-timestamp')
+    newTimestamp.textContent = obj.timestamp
+    ? obj.timestamp
+    : new Date().toLocaleString()
+
+    const newHeader = document.createElement('h3')
+    newTimestamp.classList.add('commnet-name')
+    newHeader.textContent = obj.name
+
+    const newParagraph = document.createElement('p')
+    newTimestamp.classList.add('commnet-value')
+    newParagraph.textContent = obj.value
+
+    const newComment = document.createElement('li')
+    newComment.classList.add('comment')
+    newComment.appendChild(newHeader)
+    newComment.appendChild(newTimestamp)
+    newComment.appendChild(newParagraph)
+
+    document.getElementById('comments').appendChild(newComment)
 }
 
-function newElement(elementInfo) {
-    let element = document.createElement(elementInfo.tag);
-    elementInfo.classes.forEach((classItem) => (element.classList.add(classItem)))
-    element.textContent = elementInfo.content;
-    return element
-}
-
-function displayComment(commentInfo) {
-    const commentsContainer = document.querySelector(".comments__container")
-
-    const comment = createAndAppendElement(commentsContainer, {tag:'article', classlist:
-['comment'], content: ''})
-
-    createAndAppendElement(comment, {tag: 'div', classes: ['comment__img'], content: ''})
-    createAndAppendElement(comment, {tag: 'p', classes: ['comment__name'], content:
-    commentInfo.name})
-    createAndAppendElement(comment, {tag: 'p', classes: ['comment__date'], content:
-    commentInfo.date})
-    createAndAppendElement(comment, {tag: 'p', classes: ['comment__text'], content:
-    commentInfo.comment})
-}
-
-function createComment(name, date, commentText, commentArray) {
-    commentArray.push
-}
-
-const commentInfo = [
-    {name: "Connor Walton", date: "02/17/2021", comment: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."}, 
-    {name: "Emilie Beach", date: "01/09/2021", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
-    {name: "Miles Acosta", date: "12/20/2020", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
-]
-
-commentInfo.forEach((comment) => displayComment(comment))
-
-const form = document.querySelector("form");
-
-function formSubmitHandler(event) {
+document
+    .getElementById('comment-form')
+    .addEventListener('submit', function (event) {
     event.preventDefault()
 
-    const date = new Date(Date,now())
-    const currentDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear()
+    const inputs = [
+        {
+        label: 'name',
+        elementId: 'comment-name'
+        },
+        {
+        label: 'value',
+        elementId: 'comment-text'
+        }
+    ]
 
-    createComment(event.target.name.value, Date.now(), event.target.comment.value,
-    commentInfo)
+    const value = {}
 
-    const commentsContainer = document.querySelector(".comments__container");
-    commentsContainer.innerHTML = '';
+    inputs.forEach(function (input) {
+        const inputElement = document.getElementById(input.elementId)
+        const inputValue = inputElement.value
+        inputElement.value = ''
 
-    commentInfo.forEach((comment) => displayComment(comment))
+        value[input.label] = inputValue
+    })
 
-    event.target.reset();
-}
+    createCommentElement(value)
+    })
 
-form.addEventListener('submit', formSubmitHandler);
+const commentValues = [
+    {
+    name: 'Connor Walton',
+    value: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
+    timestamp: new Date('2021-02-17').toLocaleString()
+    },
+    {
+    name: 'Emilie Beach',
+    value: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
+    timestamp: new Date('2021-01-09').toLocaleString()
+    },
+    {
+    name: 'Miles Acosta',
+    value: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+    timestamp: new Date('2020-12-20').toLocaleString()
+    }
+]
+
+const commentsList = document.getElementById('comments')
+
+commentValues.forEach(function (obj) {
+    createCommentElement(obj)
+})
+
+
+
+// commentInfo.forEach((comment) => displayComment(comment))
+
+// const form = document.querySelector("form");
+
+// function formSubmitHandler(event) {
+//     event.preventDefault()
+
+//     const date = new Date(Date,now())
+//     const currentDate = date.getDate() + "/" + date.getMonth() + "/" + date.getFullYear()
+
+//     createComment(event.target.name.value, Date.now(), event.target.comment.value,
+//     commentInfo)
+
+//     const commentsContainer = document.querySelector(".comments__container");
+//     commentsContainer.innerHTML = '';
+
+//     commentInfo.forEach((comment) => displayComment(comment))
+
+//     event.target.reset();
+// }
+
+// form.addEventListener('submit', formSubmitHandler);
 
