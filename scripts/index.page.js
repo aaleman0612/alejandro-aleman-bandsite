46665a36fa5d0145083a634fa1198c0fa7a18c6c
Alejandro Aleman <aaleman0612@gmail.com>
2023-12-10 23:30:19 -0500

    finished styling on comments section and changed the color of the gallery images and gave them the hover effect

--- scripts/index.page.js
+++ scripts/index.page.js
@@ -3,7 +3,7 @@ function createCommentElement(obj) {
     newTimestamp.classList.add('comment-timestamp')
     newTimestamp.textContent = obj.timestamp
     ? obj.timestamp
-    : new Date().toLocaleString()
+    : new Date().toLocaleDateString()
 
     const newHeader = document.createElement('h3')
     newTimestamp.classList.add('comment-name')
@@ -56,17 +56,17 @@ const commentValues = [
     {
     name: 'Connor Walton',
     value: 'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-    timestamp: new Date('2021-02-17').toLocaleString()
+    timestamp: new Date('2021-02-17').toLocaleDateString()
     },
     {
     name: 'Emilie Beach',
     value: 'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-    timestamp: new Date('2021-01-09').toLocaleString()
+    timestamp: new Date('2021-01-09').toLocaleDateString()
     },
     {
     name: 'Miles Acosta',
     value: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-    timestamp: new Date('2020-12-20').toLocaleString()
+    timestamp: new Date('2020-12-20').toLocaleDateString()
     }
 ]
 
